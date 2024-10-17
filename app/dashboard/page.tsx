7cb9e505a115ac8c5fcf23415d8106850157f677
Davagulez <davidfma@hotmail.com>
2024-10-17 16:49:46 -0300

    Modify(dashboard): h1 dinamico y correcion de valores en tablas

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -22,6 +22,15 @@ export default function Dashboard() {
     }
   }, [financialData]);
 
+  // Determinar el título de la página basado en el estado actual
+  let pageTitle = '';
+
+  if (showNewRecordForm) {
+    pageTitle = 'Generar Análisis';
+  } else if (!data) {
+    pageTitle = 'No hay datos disponibles';
+  }
+
   const handleSelectRecord = (record: FinancialData) => {
     setData(record);
     setShowNewRecordForm(false);
@@ -53,67 +62,69 @@ export default function Dashboard() {
           isSidebarExpanded={isSidebarExpanded} 
           toggleSidebar={toggleSidebar}
         />
-        <main className="flex-1 overflow-auto p-4">
-          <h1 className="text-3xl font-bold mb-4">Dashboard Financiero</h1>
-          {showNewRecordForm ? (
-            <NewRecordForm onSubmit={handleNewRecord} />
-          ) : data ? (
-            <>
-              <Card className="mb-8">
-                <CardHeader>
-                  <CardTitle>Información General</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <Table>
-                    <TableHeader>
-                      <TableRow>
-                        <TableHead>Activo</TableHead>
-                        <TableHead>Inicio</TableHead>
-                        <TableHead>Fin</TableHead>
-                        <TableHead>Creado</TableHead>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                      <TableRow>
-                        <TableCell>{data.activo}</TableCell>
-                        <TableCell>{formatYear(data.inicio)}</TableCell>
-                        <TableCell>{formatYear(data.fin)}</TableCell>
-                        <TableCell>{formatDate(data.createdAt)}</TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </CardContent>
-              </Card>
-
-              {data && Object.entries(data.resultado).map(([section, sectionData]) => (
-                <Card key={section} className="mb-8">
+        <main className="flex-1 overflow-auto p-4 flex justify-center">
+          <div className='w-3/5'>
+            {pageTitle && <h1 className="text-3xl font-bold mb-4">{pageTitle}</h1>}
+            {showNewRecordForm ? (
+              <NewRecordForm onSubmit={handleNewRecord} />
+            ) : data ? (
+              <>
+                <Card className="mb-8">
                   <CardHeader>
-                    <CardTitle className="capitalize">{section.replace('_', ' ')}</CardTitle>
+                    <CardTitle>Información General</CardTitle>
                   </CardHeader>
                   <CardContent>
                     <Table>
                       <TableHeader>
                         <TableRow>
-                          <TableHead>Métrica</TableHead>
-                          <TableHead>Valor</TableHead>
+                          <TableHead>Activo</TableHead>
+                          <TableHead>Inicio</TableHead>
+                          <TableHead>Fin</TableHead>
+                          <TableHead>Creado</TableHead>
                         </TableRow>
                       </TableHeader>
                       <TableBody>
-                        {Object.entries(sectionData as Record<string, string>).map(([metric, value]) => (
-                          <TableRow key={metric}>
-                            <TableCell className="font-medium capitalize">{metric.replace(/_/g, ' ')}</TableCell>
-                            <TableCell>{value}</TableCell>
-                          </TableRow>
-                        ))}
+                        <TableRow>
+                          <TableCell>{data.activo}</TableCell>
+                          <TableCell>{formatYear(data.inicio)}</TableCell>
+                          <TableCell>{formatYear(data.fin)}</TableCell>
+                          <TableCell>{formatDate(data.createdAt)}</TableCell>
+                        </TableRow>
                       </TableBody>
                     </Table>
                   </CardContent>
                 </Card>
-              ))}
-            </>
-          ) : (
-            <div>No hay datos disponibles</div>
-          )}
+
+                {data && Object.entries(data.resultado).map(([section, sectionData]) => (
+                  <Card key={section} className="mb-8">
+                    <CardHeader>
+                      <CardTitle className="capitalize">{section.replace('_', ' ')}</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <Table>
+                        <TableHeader>
+                          <TableRow>
+                          <TableHead className="w-3/5">Métrica</TableHead>
+                          <TableHead className="w-2/5">Valor</TableHead>
+                          </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                          {Object.entries(sectionData as Record<string, string>).map(([metric, value]) => (
+                            <TableRow key={metric}>
+                              <TableCell className="w-3/5 font-medium capitalize">{metric.replace(/_/g, ' ')}</TableCell>
+                              <TableCell className="w-2/5">{value}</TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </CardContent>
+                  </Card>
+                ))}
+              </>
+            ) : (
+              <div>No hay datos disponibles</div>
+            )}
+          </div>
         </main>
       </div>
     </div>
